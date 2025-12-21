33b55ecc8f9751c0b1cbe950eaaba29d2bd0d91f
Jefferson Hunt <jeffersonjhunt@gmail.com>
2025-12-21 17:12:18 -0600

    intial debug of neopixel

--- main/inc/main.h
+++ main/inc/main.h
@@ -1,13 +1,30 @@
 #pragma once
 
+/**
+ * @file main.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2025-12-21
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ * https://github.com/esp-cpp/espp/tree/e94ee66edcbfcb2706429d4f3e2f9c5692b7d7e8/components/qtpy
+ * https://esp-cpp.github.io/espp/qtpy.html
+ * https://github.com/adafruit/Adafruit-QT-Py-ESP32-Pico-PCB
+ */
+
 #define LOG_LOCAL_LEVEL ESP_LOG_VERBOSE
 #include "esp_log.h"
+
+#include "driver/gpio.h"
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-#include "version.h" // for versioning
+#include "neopixel.h"
 
+#include "version.h" // for versioning
 
 namespace ooe::pinled
 {
@@ -19,5 +36,7 @@ namespace ooe::pinled
 
     private:
         void version();
+        bool test1(uint32_t iterations);
+        bool test2(uint32_t iterations);
     };
 } // namespace pinled
--- main/src/main.cpp
+++ main/src/main.cpp
@@ -1,5 +1,13 @@
 #include "main.h"
 
+#define PIXEL_COUNT  5
+#define NEOPIXEL_PIN GPIO_NUM_15
+
+#if !defined(MAX)
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+#endif
+#define ARRAY_SIZE(x) (sizeof(x)/sizeof(x[0]))
+
 namespace ooe::pinled
 {
     static const char *TAG = "pinled-main"; // tag for logging
@@ -43,7 +51,9 @@ namespace ooe::pinled
     void Main::run()
     {
         ESP_LOGI(TAG, "tick");
-        vTaskDelay(1000 * 15 / portTICK_PERIOD_MS);
+        vTaskDelay(1000 * 1 / portTICK_PERIOD_MS);
+        test1(1);
+        test2(1);
     }
 
     /**
@@ -53,4 +63,69 @@ namespace ooe::pinled
     {
         ESP_LOGI(TAG, "%s version: %i.%i.%i", PROJECT_NAME, PROJECT_VERSION_MAJOR, PROJECT_VERSION_MINOR, PROJECT_VERSION_PATCH);
     }
+
+
+bool Main::test1(uint32_t iterations)
+{
+   tNeopixelContext neopixel = neopixel_Init(PIXEL_COUNT, NEOPIXEL_PIN);
+   tNeopixel pixel[] =
+   {
+       { 0, NP_RGB(50, 0,  0) }, /* red */
+       { 0, NP_RGB(0,  50, 0) }, /* green */
+       { 0, NP_RGB(0,  0, 50) }, /* blue */
+       { 0, NP_RGB(0,  0,  0) }, /* off */
+   };
+
+   if(NULL == neopixel)
+   {
+      ESP_LOGE(TAG, "[%s] Initialization failed\n", __func__);
+      return false;
+   }
+
+   ESP_LOGI(TAG, "[%s] Starting", __func__);
+   for(int iter = 0; iter < iterations; ++iter)
+   {
+      for(int i = 0; i < ARRAY_SIZE(pixel); ++i)
+      {
+         neopixel_SetPixel(neopixel, &pixel[i], 1);
+         vTaskDelay(pdMS_TO_TICKS(200));
+      }
+   }
+   ESP_LOGI(TAG, "[%s] Finished", __func__);
+
+   neopixel_Deinit(neopixel);
+   return true;
+}
+
+bool Main::test2(uint32_t iterations)
+{
+   tNeopixelContext neopixel = neopixel_Init(PIXEL_COUNT, NEOPIXEL_PIN);
+   uint32_t refreshRate, taskDelay;
+
+   if(NULL == neopixel)
+   {
+      ESP_LOGE(TAG, "[%s] Initialization failed\n", __func__);
+      return false;
+   }
+
+   refreshRate = neopixel_GetRefreshRate(neopixel);
+   taskDelay = MAX(1, pdMS_TO_TICKS(1000UL / refreshRate));
+   ESP_LOGI(TAG, "[%s] Starting", __func__);
+   for(uint32_t i = 0; i < iterations * PIXEL_COUNT; ++i)
+   {
+      tNeopixel pixel[] =
+      {
+          { (i)   % PIXEL_COUNT, NP_RGB(0, 0,  0) },
+          { (i+5) % PIXEL_COUNT, NP_RGB(0, 50, 0) }, /* green */
+      };
+      neopixel_SetPixel(neopixel, pixel, ARRAY_SIZE(pixel));
+      vTaskDelay(taskDelay);
+   }
+   ESP_LOGI(TAG, "[%s] Finished", __func__);
+   neopixel_Deinit(neopixel);
+   return true;
+}
+
+
+
 } // namespace pinled
